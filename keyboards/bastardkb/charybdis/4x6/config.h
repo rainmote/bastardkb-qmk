f8f0f4d9f0f97f2598ef6faf0ca02ba2cfe9a761
rainmote <rainmote@gmail.com>
2024-11-17 05:46:58 -0800

    remove MASTER_LEFT

--- keyboards/bastardkb/charybdis/4x6/config.h
+++ keyboards/bastardkb/charybdis/4x6/config.h
@@ -23,7 +23,7 @@
 
 /* Handedness. */
 // #define MASTER_RIGHT
-#define MASTER_LEFT
+// #define MASTER_LEFT
 
 // To use the handedness pin, resistors need to be installed on the adapter PCB.
 // If so, uncomment the following code, and undefine MASTER_RIGHT above.
