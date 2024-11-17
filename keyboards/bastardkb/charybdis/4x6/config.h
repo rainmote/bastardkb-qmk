389a54264dc32eed9fc8f9f1788b9eed52a87628
rainmote <rainmote@gmail.com>
2024-11-17 05:42:13 -0800

    master left

--- keyboards/bastardkb/charybdis/4x6/config.h
+++ keyboards/bastardkb/charybdis/4x6/config.h
@@ -22,7 +22,8 @@
 #define ROTATIONAL_TRANSFORM_ANGLE -25
 
 /* Handedness. */
-#define MASTER_RIGHT
+// #define MASTER_RIGHT
+#define MASTER_LEFT
 
 // To use the handedness pin, resistors need to be installed on the adapter PCB.
 // If so, uncomment the following code, and undefine MASTER_RIGHT above.
